82642325dd24e99e70c969eae420bd295f0c33aa
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2021-06-27 23:02:39 -0300

    Projeto Finalizado

---
+++ src/main/kotlin/br/com/rodrigo/chave/util/factory/KeyManagerGrpcClientFactory.kt
@@ -0,0 +1,41 @@
+package br.com.rodrigo.chave.util.factory
+
+import br.com.rodrigo.KeyManagerConsultaGrpcServiceGrpc
+import br.com.rodrigo.KeyManagerGrpcServiceGrpc
+import br.com.rodrigo.KeyManagerListaChavesGrpcServiceGrpc
+import br.com.rodrigo.KeyManagerRemoveGrpcServiceGrpc
+import io.grpc.ManagedChannel
+import io.micronaut.context.annotation.Factory
+import io.micronaut.grpc.annotation.GrpcChannel
+import javax.inject.Singleton
+
+
+@Factory
+class KeyManagerGrpcClientFactory(@GrpcChannel("key-manager-grpc-channel") val channel: ManagedChannel) {
+
+    /*
+    Essa é a fábrica de Client do serviço de Registrar Chave Pix
+    Essa função foi escrita de maneira extensa, igual escrevemos no Java
+    A seguir escrevemos a mesma função usando o recurso que o Kotlin ->
+    Função com apenas um retorno (somente 1 branch) podemos retornar logo após a assinatura da função.
+
+    @Singleton
+    fun keyManagerGrpcStub(): KeyManagerGrpcServiceGrpc.KeyManagerGrpcServiceBlockingStub {
+        return KeyManagerGrpcServiceGrpc.newBlockingStub(channel)
+    }
+     */
+
+    //Essa é a fábrica de Client do serviço de Registrar Chave Pix
+
+    @Singleton
+    fun keyManagerGrpcStub() = KeyManagerGrpcServiceGrpc.newBlockingStub(channel)
+
+    @Singleton
+    fun keyManagerRemoveGrpcStub() = KeyManagerRemoveGrpcServiceGrpc.newBlockingStub(channel)
+
+    @Singleton
+    fun keyManagerDetalheGrpcStub() = KeyManagerConsultaGrpcServiceGrpc.newBlockingStub(channel)
+
+    @Singleton
+    fun keyManagerListaGrpcStub() = KeyManagerListaChavesGrpcServiceGrpc.newBlockingStub(channel)
+}
